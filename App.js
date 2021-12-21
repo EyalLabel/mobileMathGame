9d9f89ad6a9040563fec814c0b31fbc87cc28788
Eyal Label <sslabel2@gmail.com>
2021-12-21 15:59:08 +0200

    commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, SafeAreaView,View } from 'react-native';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Future math game!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
